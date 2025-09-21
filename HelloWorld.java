c8a1ef38b4228cbdad8ed4fb75dc67f58c241ef5
Atthaya Makayla Rosandi <atthayamakaylarosandi06@gmail.com>
2025-09-21 12:51:05 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// nama: kayla
+// nim: 2511016220023
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // kode program
+        System.out.println("Hello World");
+        System.out.print("Nama: ");
+        System.out.println("Atthaya Makayla Rosandi ");
+        System.out.print("NIM: ");
+        System.out.println("2511016220023");
+        System.out.print("Umur: ");
+        System.out.println( 18);
+    }
+}
